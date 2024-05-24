bc04e9c47ed36b59d52f64563925a7bcd7f04fe1
antle9053 <antle9053@gmail.com>
2024-05-24 14:54:17 +0700

    Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+  {
+    path: "/content",
+    element: <div>Content</div>,
+  },
+  {
+    path: "/products/:id",
+    element: <div>Products</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
